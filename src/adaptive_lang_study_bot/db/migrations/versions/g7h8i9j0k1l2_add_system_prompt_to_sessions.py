f1af4af82e7ec2a09ba936eff5d6b64c1c9c49f3
Sergei Bratchikov <hivaze.me@gmail.com>
2026-02-28 01:52:51 +0000

    Fix migration revision ID collision (a1b2c3d4e5f6 was already used)

--- src/adaptive_lang_study_bot/db/migrations/versions/g7h8i9j0k1l2_add_system_prompt_to_sessions.py
+++ src/adaptive_lang_study_bot/db/migrations/versions/g7h8i9j0k1l2_add_system_prompt_to_sessions.py
@@ -1,6 +1,6 @@
 """add system_prompt to sessions
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: g7h8i9j0k1l2
 Revises: f6a7b8c9d0e1
 Create Date: 2026-02-28 12:00:00.000000
 
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision: str = "a1b2c3d4e5f6"
+revision: str = "g7h8i9j0k1l2"
 down_revision: Union[str, Sequence[str], None] = "f6a7b8c9d0e1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
